682b8b29e115b2b1cce30b315c744f5dbaf875a6
tantohtelmah <tantohtelmah@gmail.com>
2023-12-15 00:07:19 -0500

    tasks 0,1,2,3

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,10 +13,8 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int tmp;
 
-	if (size == 0 || array == NULL)
-		return;
 	if (size < 2)
-		print_array(array, size);
+		return;
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
